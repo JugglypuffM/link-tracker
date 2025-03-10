c2412e9b2db43915b2c8b905f9c32fe4fba31cdb
JugglypuffM <stas.klinin@gmail.com>
2025-03-10 22:54:07 +0500

    fix: десериализация Uri в scrapper-е

--- scrapper/src/main/scala/domain/UriReader.scala
+++ scrapper/src/main/scala/domain/UriReader.scala
@@ -1,8 +1,9 @@
 package domain
 
+import sttp.client3.UriContext
 import sttp.model.Uri
 import tethys.JsonReader
 
 object UriReader {
-  given JsonReader[Uri] = JsonReader.stringReader.map(Uri.apply)
+  given JsonReader[Uri] = JsonReader.stringReader.map(str => uri"$str")
 }
